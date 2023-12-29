e971448bd9906506c4a80c5dce0b3479898a6c63
sachinchavda17 <sachinchavda321@gmail.com>
2023-12-29 23:12:58 +0530

    config added

---
+++ portfolio_frontend/src/utils/config.js
@@ -0,0 +1,4 @@
+export const cloudinary_upload_preset =  process.env.REACT_APP_CLUDINARY_UPLOAD_PRESET;
+export const RESUME_URL =  process.env.REACT_APP_RESUME_URL
+export const GithubRepo =  process.env.REACT_APP_GITHUB_REPOSITORY
+export const backendUrl =  process.env.REACT_APP_BACKEND_URL
